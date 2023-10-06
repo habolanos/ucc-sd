09135ef73b21b64687e5039a355870bd63538cce
Externo Tinet40 <externo.tinet40@consorcio.cl>
2023-10-05 20:13:54 -0500

    Terraform's First class

---
+++ terraform-basic/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "server-frontend"
+  ports {
+    internal = 80
+    external = 9000
+  }
+}
